1564561d53bf671da129c896ff9f50e9a4e7b88c
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2024-03-18 11:53:05 +0000

    Implement DEL command

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,17 @@ use command::Parser;
 enum Value {
     String(Vec<u8>),
     List(Vec<Vec<u8>>),
+    Number(i64),
     Nil,
 }
 
 fn execute_command(state: &mut HashMap<Vec<u8>, Value>, cmd: Vec<Vec<u8>>) -> anyhow::Result<Value> {
     println!("Command: {:?}", cmd);
     let value = match cmd[0].as_slice() {
+        b"DEL" => {
+            let removed = cmd[1..].iter().filter(|&key| state.remove(key).is_some()).count();
+            Value::Number(removed as _)
+        }
         b"GET" => {
             let [_, key] = cmd.try_into().map_err(|_| anyhow::anyhow!("expected GET key"))?;
             match state.get(&key) {
@@ -51,6 +56,7 @@ fn write_response(writer: &mut impl Write, res: anyhow::Result<Value>) -> anyhow
                 write!(writer, "\r\n")?;
             }
         }
+        Ok(Value::Number(value)) => write!(writer, ":{value}\r\n")?,
         Ok(Value::Nil) => write!(writer, "$-1\r\n")?,
         Err(e) => write!(writer, "-ERR {e}\r\n")?,
     }
@@ -60,7 +66,6 @@ fn write_response(writer: &mut impl Write, res: anyhow::Result<Value>) -> anyhow
 
 fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind(("0.0.0.0", 8888))?;
-
     let mut state = HashMap::new();
     for stream in listener.incoming() {
         let stream = stream?;
@@ -84,22 +89,25 @@ fn main() -> anyhow::Result<()> {
 mod tests {
     use super::*;
 
+    macro_rules! exec_cmd {
+        ($state:expr, $($cmd:expr),+) => {
+            execute_command(&mut $state, vec![$($cmd.as_bytes().to_vec(),)+]).unwrap()
+        }
+    }
+
     #[test]
     fn test_get_set() {
         let mut state = HashMap::new();
-        let res = execute_command(&mut state, vec![b"GET".to_vec(), b"a".to_vec()]).unwrap();
-        assert_eq!(res, Value::Nil);
+        assert_eq!(exec_cmd!(state, "GET", "a"), Value::Nil);
 
-        let res = execute_command(&mut state, vec![b"SET".to_vec(), b"a".to_vec(), b"b".to_vec()]).unwrap();
-        assert_eq!(res, Value::String(b"OK".to_vec()));
+        assert_eq!(exec_cmd!(state, "SET", "a", "b"), Value::String(b"OK".to_vec()));
+        assert_eq!(exec_cmd!(state, "GET", "a"), Value::String(b"b".to_vec()));
 
-        let res = execute_command(&mut state, vec![b"GET".to_vec(), b"a".to_vec()]).unwrap();
-        assert_eq!(res, Value::String(b"b".to_vec()));
+        assert_eq!(exec_cmd!(state, "SET", "a", "c"), Value::String(b"OK".to_vec()));
+        assert_eq!(exec_cmd!(state, "GET", "a"), Value::String(b"c".to_vec()));
 
-        let res = execute_command(&mut state, vec![b"SET".to_vec(), b"a".to_vec(), b"c".to_vec()]).unwrap();
-        assert_eq!(res, Value::String(b"OK".to_vec()));
-
-        let res = execute_command(&mut state, vec![b"GET".to_vec(), b"a".to_vec()]).unwrap();
-        assert_eq!(res, Value::String(b"c".to_vec()));
+        assert_eq!(exec_cmd!(state, "DEL", "a"), Value::Number(1));
+        assert_eq!(exec_cmd!(state, "DEL", "b"), Value::Number(0));
+        assert_eq!(exec_cmd!(state, "DEL", "a", "b", "c"), Value::Number(0));
     }
 }
