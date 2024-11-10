0238193061329ddeb552ec7241fc2ff2d2b7f831
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2024-11-10 19:01:31 +0100

    add tests for rpop

--- src/commands/rpop.rs
+++ src/commands/rpop.rs
@@ -21,8 +21,25 @@ pub fn run(db: &mut Database, mut cmd: Command) -> anyhow::Result<Response> {
         Some(n) if n < 0 => anyhow::bail!("value is out of range, must be positive"),
         Some(n) => {
             let n = list.len().min(n as _);
-            Response::Array(list.split_off(n))
+            let v = (0..n).map(|_| list.pop().unwrap()).collect();
+            Response::Array(v)
         }
         None => list.pop().map(|v| Response::String(v)).unwrap_or(Response::Nil),
     })
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::redis_test;
+
+    redis_test! {
+        test_rpop
+        "rpop x"            => ();
+        "rpush x 1 2 3 4 5" => 5;
+        "rpop x"            => "5";
+        "lrange x 0 -1"     => ["1", "2", "3", "4"];
+        "rpop x 3"          => ["4", "3", "2"];
+        "rpop x 10"         => ["1"];
+        "llen x"            => 0;
+    }
 }
