633dad3823fab422eb0458a26333b651bb2a7e86
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2024-11-10 17:56:43 +0100

    add tests for del

--- src/commands/del.rs
+++ src/commands/del.rs
@@ -18,3 +18,20 @@ pub fn run(db: &mut Database, mut cmd: Command) -> anyhow::Result<Response> {
     anyhow::ensure!(!keys.is_empty(), "expected DEL key [key ...]");
     Ok(Response::Number(keys.iter().filter(|&key| db.del(key).is_some()).count() as _))
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::redis_test;
+
+    redis_test! {
+        test_del
+        "del x y z"    => 0;
+        "set x 0"      => "OK";
+        "del x"        => 1;
+        "set x 1"      => "OK";
+        "set y 2"      => "OK";
+        "set z 3"      => "OK";
+        "del x y z"    => 3;
+        "exists x y z" => 0;
+    }
+}
