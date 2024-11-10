b16b96f7cc51a95c13ff2a62c13c5331dad276e9
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2024-11-10 17:56:43 +0100

    add tests for decr

--- src/commands/decr.rs
+++ src/commands/decr.rs
@@ -19,3 +19,16 @@ pub fn run(db: &mut Database, mut cmd: Command) -> anyhow::Result<Response> {
     let key = cmd.parse_args::<ByteString>()?;
     incr_by(db, key, -1)
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::redis_test;
+
+    redis_test! {
+        test_decr
+        "decr x"     => -1;
+        "decr x"     => -2;
+        "set x 1234" => "OK";
+        "decr x"     => 1233;
+    }
+}
