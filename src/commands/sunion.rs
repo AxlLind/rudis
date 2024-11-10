1199abd0b2d36c53f0841090a7f2060c65b9bade
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2024-11-10 19:49:09 +0100

    add tests for sunion

--- src/commands/sunion.rs
+++ src/commands/sunion.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use super::CommandInfo;
 use crate::cmd_parser::Command;
 use crate::{ByteString, Database, Response};
@@ -14,8 +16,9 @@ pub static INFO: CommandInfo = CommandInfo {
 };
 
 pub fn run(db: &mut Database, mut cmd: Command) -> anyhow::Result<Response> {
-    let (key, keys) = cmd.parse_args::<(ByteString, Vec<ByteString>)>()?;
-    let Some(mut set) = db.get_set(&key)?.cloned() else { return Ok(Response::Array(Vec::new())) };
+    let keys = cmd.parse_args::<Vec<ByteString>>()?;
+    anyhow::ensure!(!keys.is_empty(), "expected SUNION key [key ...]");
+    let mut set = HashSet::new();
     for k in keys {
         let Some(s) = db.get_set(&k)? else { continue };
         for m in s.iter() {
@@ -27,4 +30,23 @@ pub fn run(db: &mut Database, mut cmd: Command) -> anyhow::Result<Response> {
     let mut elems = set.into_iter().collect::<Vec<_>>();
     elems.sort();
     Ok(Response::Array(elems))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::redis_test;
+
+    redis_test! {
+        test_sunion
+        "sadd x 1 2 3" => 3;
+        "sadd y 2 3 4" => 3;
+        "sadd z 3 4 5" => 3;
+        "sunion x"     => ["1", "2", "3"];
+        "sunion x z"   => ["1", "2", "3", "4", "5"];
+        "sunion x y z" => ["1", "2", "3", "4", "5"];
+        "sunion x y"   => ["1", "2", "3", "4"];
+        "sunion y z"   => ["2", "3", "4", "5"];
+        "sunion q"     => [];
+        "sunion q x"   => ["1", "2", "3"];
+    }
 }
