0e428633abec8f83f6e389f841b8436483c2c6b7
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2024-11-10 19:42:45 +0100

    add tests for srem

--- src/commands/srem.rs
+++ src/commands/srem.rs
@@ -21,3 +21,20 @@ pub fn run(db: &mut Database, mut cmd: Command) -> anyhow::Result<Response> {
     let removed = members.iter().filter(|&m| set.remove(m)).count();
     Ok(Response::Number(removed as _))
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::redis_test;
+
+    redis_test! {
+        test_srem
+        "sadd x 1 2 3" => 3;
+        "srem x 1"     => 1;
+        "smembers x"   => ["2", "3"];
+        "srem x 1"     => 0;
+        "srem x 0 1 7" => 0;
+        "srem x 2 9 8" => 1;
+        "smembers x"   => ["3"];
+        "srem q 1"     => 0;
+    }
+}
