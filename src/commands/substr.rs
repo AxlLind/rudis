55d065517162a7db398f9df0f5df56982e19a730
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2024-11-11 22:22:21 +0100

    implement substr command

--- src/commands/substr.rs
+++ src/commands/substr.rs
@@ -1,6 +1,6 @@
-use super::CommandInfo;
+use super::{clamp_range, CommandInfo};
 use crate::cmd_parser::Command;
-use crate::{Database, Response};
+use crate::{ByteString, Database, Response};
 
 pub static INFO: CommandInfo = CommandInfo {
     name: b"substr",
@@ -13,6 +13,28 @@ pub static INFO: CommandInfo = CommandInfo {
     step: 1,
 };
 
-pub fn run(_: &mut Database, _: Command) -> anyhow::Result<Response> {
-    anyhow::bail!("unimplemented")
+pub fn run(db: &mut Database, mut cmd: Command) -> anyhow::Result<Response> {
+    let (key, start, stop) = cmd.parse_args::<(ByteString, i64, i64)>()?;
+    Ok(match db.get_str(&key)? {
+        Some(list) => {
+            let (start, stop) = clamp_range(list.len(), start, stop);
+            Response::String(list[start..=stop].to_vec())
+        }
+        None => Response::String(Vec::new()),
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::redis_test;
+
+    redis_test! {
+        test_strlen
+        "set x this_is_a_string" => "OK";
+        "substr x 0 3"           => "this";
+        "substr x -3 -1"         => "ing";
+        "substr x 0 -1"          => "this_is_a_string";
+        "substr x 10 100"        => "string";
+        "substr q 0 -1"          => "";
+    }
 }
