3d6ec85b9d622422248b36b8fe8e36ac36a72779
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2024-11-13 21:34:52 +0100

    implement hgetall command

--- src/commands/hgetall.rs
+++ src/commands/hgetall.rs
@@ -1,6 +1,6 @@
 use super::CommandInfo;
 use crate::cmd_parser::Command;
-use crate::{Database, Response};
+use crate::{ByteString, Database, Response};
 
 pub static INFO: CommandInfo = CommandInfo {
     name: b"hgetall",
@@ -13,6 +13,29 @@ pub static INFO: CommandInfo = CommandInfo {
     step: 1,
 };
 
-pub fn run(_: &mut Database, _: Command) -> anyhow::Result<Response> {
-    anyhow::bail!("unimplemented")
+pub fn run(db: &mut Database, mut cmd: Command) -> anyhow::Result<Response> {
+    let key = cmd.parse_args::<ByteString>()?;
+    let pairs = match db.get_hash(&key)? {
+        Some(h) => {
+            let mut pairs = h.iter().collect::<Vec<_>>();
+            pairs.sort();
+            pairs.into_iter().flat_map(|(k, v)| [k, v]).cloned().collect()
+        },
+        None => Vec::new(),
+    };
+    Ok(Response::Array(pairs))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::redis_test;
+
+    redis_test! {
+        test_hgetall
+        "hset x a b x y" => 2;
+        "hgetall x" => ["a", "b", "x", "y"];
+        "hdel x a"  => 1;
+        "hgetall x" => ["x", "y"];
+        "hgetall q" => [];
+    }
 }
