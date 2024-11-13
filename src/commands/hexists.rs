2c8888fda6fc0c4a18a14b191852750a00a39791
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2024-11-13 21:26:22 +0100

    implement hexists command

--- src/commands/hexists.rs
+++ src/commands/hexists.rs
@@ -1,6 +1,6 @@
 use super::CommandInfo;
 use crate::cmd_parser::Command;
-use crate::{Database, Response};
+use crate::{ByteString, Database, Response};
 
 pub static INFO: CommandInfo = CommandInfo {
     name: b"hexists",
@@ -14,6 +14,22 @@ pub static INFO: CommandInfo = CommandInfo {
     step: 1,
 };
 
-pub fn run(_: &mut Database, _: Command) -> anyhow::Result<Response> {
-    anyhow::bail!("unimplemented")
+pub fn run(db: &mut Database, mut cmd: Command) -> anyhow::Result<Response> {
+    let (key, field) = cmd.parse_args::<(ByteString, ByteString)>()?;
+    let exists = db.get_hash(&key)?.map(|h| h.contains_key(&field)).unwrap_or(false);
+    Ok(Response::Number(exists as _))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::redis_test;
+
+    redis_test! {
+        test_hexists
+        "hset x a b x y" => 2;
+        "hexists x a" => 1;
+        "hexists x x" => 1;
+        "hexists x b" => 0;
+        "hexists q r" => 0;
+    }
 }
