3c82e7fa3b118fb50214f300b3582f24d2d5504b
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2024-11-13 21:51:40 +0100

    implement hstrlen command

--- src/commands/hstrlen.rs
+++ src/commands/hstrlen.rs
@@ -1,6 +1,6 @@
 use super::CommandInfo;
 use crate::cmd_parser::Command;
-use crate::{Database, Response};
+use crate::{ByteString, Database, Response};
 
 pub static INFO: CommandInfo = CommandInfo {
     name: b"hstrlen",
@@ -14,6 +14,22 @@ pub static INFO: CommandInfo = CommandInfo {
     step: 1,
 };
 
-pub fn run(_: &mut Database, _: Command) -> anyhow::Result<Response> {
-    anyhow::bail!("unimplemented")
+pub fn run(db: &mut Database, mut cmd: Command) -> anyhow::Result<Response> {
+    let (key, field) = cmd.parse_args::<(ByteString, ByteString)>()?;
+    let len = db.get_hash(&key)?.and_then(|h| h.get(&field)).map(|s| s.len()).unwrap_or(0);
+    Ok(Response::Number(len as _))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::redis_test;
+
+    redis_test! {
+        test_hstrlen
+        "hset x a b y abc"   => 2;
+        "hstrlen x a"        => 1;
+        "hstrlen x y"        => 3;
+        "hstrlen x q"        => 0;
+        "hstrlen q r"        => 0;
+    }
 }
