7d9936a37459163514f34cfad418307db8da3346
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2024-11-25 19:16:48 +0100

    handle multiple connections in server

--- rudis-server/src/main.rs
+++ rudis-server/src/main.rs
@@ -1,45 +1,68 @@
 use anyhow::{self, Context};
+use smol::channel::{Receiver, Sender};
 
 use std::io::Write;
-use rudis::{Database, execute_command, write_response};
+use rudis::{execute_command, write_response, Command, Database, Response};
 use smol::net::{TcpListener, TcpStream};
-use smol::io::{AsyncWriteExt, BufReader};
+use smol::io::{AsyncRead, AsyncWriteExt, BufReader};
 
 mod cmd_parser;
 use cmd_parser::Parser;
 
-async fn handle_connection(stream: TcpStream) -> anyhow::Result<()> {
-    let mut parser = Parser::new(BufReader::new(stream.clone()));
-    let mut writer = stream;
-    let mut db = Database::default();
-    let mut buf = Vec::new();
+async fn read_command_task<R: AsyncRead + Unpin>(
+    mut parser: Parser<R>,
+    db_tx: Sender<(Sender<anyhow::Result<Response>>, Command)>,
+    tx: Sender<anyhow::Result<Response>>,
+) -> anyhow::Result<()> {
     loop {
-        let r = match parser.read_command().await {
+        match parser.read_command().await {
             Ok(cmd) => {
                 println!("Got command: {}", cmd);
-                if cmd.cmd() == "quit" {
-                    break;
-                }
-                execute_command(&mut db, cmd)
-            },
-            Err(e) => Err(e),
-        };
+                db_tx.send((tx.clone(), cmd)).await?;
+            }
+            Err(e) => tx.send(Err(e)).await?,
+        }
+    }
+}
+
+async fn send_response_task(mut stream: TcpStream, rx: Receiver<anyhow::Result<Response>>) -> anyhow::Result<()> {
+    let mut buf = Vec::new();
+    loop {
         buf.clear();
-        match r {
+        match rx.recv().await? {
             Ok(res) => write_response(&mut buf, res),
             Err(e) => write!(&mut buf, "-ERR {e}\r\n").with_context(|| "failed to write error"),
         }.unwrap();
-        writer.write_all(&buf).await?;
+        stream.write_all(&buf).await?;
     }
-    Ok(())
+}
+
+async fn handle_connection(stream: TcpStream, db_tx: Sender<(Sender<anyhow::Result<Response>>, Command)>) {
+    let parser = Parser::new(BufReader::new(stream.clone()));
+    let (tx, rx) = smol::channel::bounded(128);
+    let _ = smol::future::zip(
+        read_command_task(parser, db_tx, tx),
+        send_response_task(stream, rx),
+    ).await;
+}
+
+async fn database_task(db_rx: Receiver<(Sender<anyhow::Result<Response>>, Command)>) -> anyhow::Result<()> {
+    let mut db = Database::default();
+    loop {
+        let (tx, cmd) = db_rx.recv().await?;
+        let res = execute_command(&mut db, cmd);
+        tx.send(res).await?;
+    }
 }
 
 fn main() -> anyhow::Result<()> {
     smol::block_on(async {
         let listener = TcpListener::bind(("0.0.0.0", 8888)).await?;
+        let (db_tx, db_rx) = smol::channel::bounded(1024);
+        smol::spawn(database_task(db_rx)).detach();
         loop {
-            let (stream, _addr) = listener.accept().await?;
-            smol::spawn(handle_connection(stream)).detach();
+            let (stream, _) = listener.accept().await?;
+            smol::spawn(handle_connection(stream, db_tx.clone())).detach();
         }
     })
 }
