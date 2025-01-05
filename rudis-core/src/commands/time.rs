0422c4a15175600c06a2c8e3762f0e3de31c9e85
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2025-01-05 09:49:30 +0100

    core: remove unnecessary vec

--- rudis-core/src/commands/time.rs
+++ rudis-core/src/commands/time.rs
@@ -21,7 +21,7 @@ pub fn run(_: &mut Database, cmd: Command) -> anyhow::Result<Response> {
     let t = SystemTime::now().duration_since(UNIX_EPOCH).expect("now is later than unix epoch");
     let s = t.as_secs().to_string().into_bytes();
     let ms = t.subsec_micros().to_string().into_bytes();
-    Ok(Response::string_array(vec![s, ms]))
+    Ok(Response::string_array([s, ms]))
 }
 
 // TODO: how to test this?
