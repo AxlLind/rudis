726d914207d991bbab19d5fc3bddb499d3bc8045
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2026-03-08 09:14:06 +0100

    tools: simplify add-cmd.py

--- tools/add-cmd.py
+++ tools/add-cmd.py
@@ -3,13 +3,12 @@ import argparse
 import json
 import subprocess
 from pathlib import Path
-from typing import Any
 
 REPO_ROOT = (Path(__file__).parent / '..').resolve()
 
 SRC = """
 use super::CommandInfo;
-use crate::cmd_parser::Command;
+use crate::command::Command;
 use crate::{{ByteString, Database, Response, Value}};
 
 pub static INFO: CommandInfo = CommandInfo {{
@@ -33,10 +32,24 @@ crate::command_test! {{
 """.lstrip()
 
 
-def instantiate_cmd(info: list[Any]) -> None:
-    name, arity, flags, first_key, last_key, step, *_ = info
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command', type=lambda s: s.lower(), help='command to instantiate')
+    opts = parser.parse_args()
+
+    res = subprocess.run(
+        ['redis-cli', '--json', 'command', 'info', opts.command],
+        stdout=subprocess.PIPE,
+        text=True,
+        check=True,
+    )
+    command_info = json.loads(res.stdout)[0]
+    assert command_info is not None, f'invalid command "{opts.command}"'
+
+    name, arity, flags, first_key, last_key, step, *_ = command_info
     f = REPO_ROOT / 'rudis-core' / 'src' / 'commands' / f'{name}.rs'
-    print(f'instantiating: {f.relative_to(REPO_ROOT)}')
+
+    print(f'[+] instantiating: {f.relative_to(REPO_ROOT)}')
     flags = "\n        ".join(f'b"{f}",' for f in flags)
     if flags:
         flags = f'\n        {flags}\n    '
@@ -51,30 +64,13 @@ def instantiate_cmd(info: list[Any]) -> None:
         )
     )
 
-    print('adding to command list in mod.rs')
+    print('[+] adding to command list in mod.rs')
     modrs = REPO_ROOT / 'rudis-core' / 'src' / 'commands' / 'mod.rs'
     lines = modrs.read_text().splitlines()
     i = lines.index('register_commands! {') + 1
     j = i + lines[i:].index('}')
     commands = sorted(lines[i:j] + [f'    {name},'], key=lambda c: c.strip().removeprefix('r#'))
     modrs.write_text('\n'.join(lines[:i] + commands + lines[j:]) + '\n')
-
-
-def main() -> None:
-    parser = argparse.ArgumentParser()
-    parser.add_argument('command', type=lambda s: s.lower(), help='command to instantiate')
-    opts = parser.parse_args()
-
-    res = subprocess.run(
-        ['redis-cli', '--json', 'command', 'info', opts.command],
-        stdout=subprocess.PIPE,
-        text=True,
-        check=True,
-    )
-    command_info = json.loads(res.stdout)[0]
-    if command_info is None:
-        raise Exception(f'invalid command "{opts.command}"')
-    instantiate_cmd(command_info)
 
 
 if __name__ == "__main__":
