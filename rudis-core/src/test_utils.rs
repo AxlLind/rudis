241fc33d05db497beaeefe9604ebc684fd0094fc
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2026-03-12 22:21:07 +0100

    simplify test utils

--- rudis-core/src/test_utils.rs
+++ rudis-core/src/test_utils.rs
@@ -3,7 +3,6 @@ use crate::Response;
 
 pub trait AsResponse: Sized {
     fn as_response(s: Self) -> Response;
-    fn as_response_in_array(s: Self) -> Response { Self::as_response(s) }
 }
 
 impl AsResponse for () {
@@ -11,8 +10,7 @@ impl AsResponse for () {
 }
 
 impl AsResponse for &str {
-    fn as_response(s: Self) -> Response { Response::SimpleString(s.to_string().into_bytes()) }
-    fn as_response_in_array(s: Self) -> Response { Response::BulkString(s.to_string().into_bytes()) }
+    fn as_response(s: Self) -> Response { Response::BulkString(s.to_string().into_bytes()) }
 }
 
 impl AsResponse for i64 {
@@ -26,7 +24,6 @@ impl AsResponse for f64 {
 pub fn _sort_response(r: &mut Response) {
     let Response::Array(v) = r else { return };
     v.sort_by(|a, b| match (a, b) {
-        (Response::Nil, Response::Nil) => Ordering::Equal,
         (Response::Number(x), Response::Number(y)) => x.cmp(y),
         (Response::SimpleString(x), Response::SimpleString(y)) => x.cmp(y),
         (Response::BulkString(x), Response::BulkString(y)) => x.cmp(y),
@@ -40,25 +37,25 @@ macro_rules! command_test {
         let cmd = $crate::Command::new($cmd.split(' ').map(|w| w.as_bytes().to_vec()).collect()).unwrap();
         let res = $crate::execute_command(&mut $db, cmd).unwrap();
         let expected = $expected;
-        match (res, &expected) {
-            (Response::BulkString(s), Response::SimpleString(_)) => {
-                assert_eq!(Response::SimpleString(s), expected, $cmd);
+        match (res, expected) {
+            (Response::SimpleString(res), Response::BulkString(expected)) => {
+                assert_eq!(Response::SimpleString(res), Response::SimpleString(expected), $cmd);
             }
-            (res, _) => assert_eq!(res, expected, $cmd),
+            (res, expected) => assert_eq!(res, expected, $cmd),
         }
         $crate::command_test!{ @expand_tests $db $($rest)* }
     };
     (@expand_tests $db:ident $cmd:literal => [$($expected:expr),* $(,)?] ignore_order; $($rest:tt)*) => {
         let cmd = $crate::Command::new($cmd.split(' ').map(|w| w.as_bytes().to_vec()).collect()).unwrap();
         let mut res = $crate::execute_command(&mut $db, cmd).unwrap();
-        let mut expected = Response::Array(vec![$($crate::test_utils::AsResponse::as_response_in_array($expected)),*]);
+        let mut expected = Response::Array(vec![$($crate::test_utils::AsResponse::as_response($expected)),*]);
         $crate::test_utils::_sort_response(&mut res);
         $crate::test_utils::_sort_response(&mut expected);
         assert_eq!(res, expected, $cmd);
         $crate::command_test!{ @expand_tests $db $($rest)* }
     };
     (@expand_tests $db:ident $cmd:literal => [$($expected:expr),* $(,)?]; $($rest:tt)*) => {
-        $crate::command_test!(@expand_test $db $cmd => Response::Array(vec![$($crate::test_utils::AsResponse::as_response_in_array($expected)),*]); $($rest)*);
+        $crate::command_test!(@expand_test $db $cmd => Response::Array(vec![$($crate::test_utils::AsResponse::as_response($expected)),*]); $($rest)*);
     };
     (@expand_tests $db:ident $cmd:literal => $expected:expr; $($rest:tt)*) => {
         $crate::command_test!(@expand_test $db $cmd => $crate::test_utils::AsResponse::as_response($expected); $($rest)*);
